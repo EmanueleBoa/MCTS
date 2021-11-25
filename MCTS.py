621b60fcbe13520450e9b7269f2d59d501ac045d
Emanuele Boattini <daitto.ai@gmail.com>
2021-11-25 12:13:11 +0100

    Created MCTS.py

---
+++ MCTS.py
@@ -0,0 +1,13 @@
+import numpy as np
+from collections import defaultdict
+
+calss Node():
+    def __init__ (self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.number_of_visits = 0
+        self.results = defaultdict(int)
+        self.results[1] = 0
+        self.results[-1] = 0
+        self.untried_actions = None
